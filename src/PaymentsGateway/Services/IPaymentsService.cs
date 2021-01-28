fa22366f45c80e87ccc82c570bce543c6d3244cd
Daniel M <13464314+dmutsvanemoto@users.noreply.github.com>
2021-01-28 15:15:56 +0000

    refactore ipaymentssevice

--- src/PaymentsGateway/Services/IPaymentsService.cs
+++ src/PaymentsGateway/Services/IPaymentsService.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using PaymentsGateway.HttpClients;
 using PaymentsGateway.Models;
 
 namespace PaymentsGateway.Services
@@ -10,32 +9,5 @@ namespace PaymentsGateway.Services
         Task<bool> ProcessPayment(PaymentRequest payload);
 
         Task<IList<PaymentsContract>> RetrievePayments(int merchantId);
-    }
-
-    public class PaymentService : IPaymentsService
-    {
-        private readonly IPaymentsApiClient _paymentsApiClient;
-
-        public PaymentService(IPaymentsApiClient paymentsApiClient)
-        {
-            _paymentsApiClient = paymentsApiClient;
-        }
-
-        public async Task<bool> ProcessPayment(PaymentRequest payload)
-        {
-            var response = await _paymentsApiClient.CreatePayment(payload);
-
-            if (response.IsSuccessStatusCode)
-            {
-                return true;
-            }
-
-            return false;
-        }
-
-        public Task<IList<PaymentsContract>> RetrievePayments(int merchantId)
-        {
-            throw new System.NotImplementedException();
-        }
     }
 }
---
+++ src/PaymentsGateway/Services/PaymentService.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using PaymentsGateway.HttpClients;
+using PaymentsGateway.Models;
+
+namespace PaymentsGateway.Services
+{
+    public class PaymentService : IPaymentsService
+    {
+        private readonly IPaymentsApiClient _paymentsApiClient;
+
+        public PaymentService(IPaymentsApiClient paymentsApiClient)
+        {
+            _paymentsApiClient = paymentsApiClient;
+        }
+
+        public async Task<bool> ProcessPayment(PaymentRequest payload)
+        {
+            var response = await _paymentsApiClient.CreatePayment(payload);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public Task<IList<PaymentsContract>> RetrievePayments(int merchantId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
